2a77843ec00b9abf1d382d9ebfdc8eeffb0007cd
Kevin Branigan <kbranigan@gmail.com>
2018-02-04 02:54:49 -0500

    added an embedded example and test

---
+++ examples/test_routes_embedded.c
@@ -0,0 +1,18 @@
+
+#include "routes.h"
+#include <stdio.h>
+
+int main()
+{
+  // int num_routes = 0;
+  // struct s_routes * routes = NULL;
+  // routes = load_routes(&num_routes);
+  printf("num_routes = %d\n", num_routes);
+
+  for (int i = 0 ; i < num_routes ; i++)
+  {
+    printf("%d: %s\n", i, routes[i].route_long_name);
+  }
+
+  printf("done\n");
+}
