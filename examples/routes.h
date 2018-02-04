9222d5941d0216512d5ecf99582f3e127d4f09c6
Kevin Branigan <kbranigan@gmail.com>
2018-02-04 02:59:14 -0500

    decided to add back example output so it can be viewed from GitHub

---
+++ examples/routes.h
@@ -0,0 +1,21 @@
+// generated automatically from /Users/kevin/work/branigan.ca/prototypes/csv_to_chb/examples/routes.csv
+
+#ifndef GENERATED_AUTOMATICALLY_FROM_routes_CSV
+#define GENERATED_AUTOMATICALLY_FROM_routes_CSV
+
+struct s_routes {
+  int   route_id;
+  int   agency_id;
+  int   route_short_name;
+  char  route_long_name[28];
+  int   route_type;
+  char  route_color[8];
+  char  route_text_color[8];
+};
+
+extern struct s_routes * load_routes(int * ret_count);
+
+extern int num_routes;
+extern struct s_routes routes[20];
+
+#endif // GENERATED_AUTOMATICALLY_FROM_routes_CSV
